d415693993d0794ad8e61c08e83ea2e6f3641a67
uriash83 <30936957+uriash83@users.noreply.github.com>
2020-09-21 14:13:24 +0200

    Update settings.py

--- analyze/settings.py
+++ analyze/settings.py
@@ -151,8 +151,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR,'staticfiles')
-STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
